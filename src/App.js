fbef329f1557b3800988419de20ecdb1087dbd35
Sebastian Arrieta <sarrietav@unicartagena.edu.co>
2020-11-28 21:32:52 -0500

    Added Editor component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
+import Editor from "./components/Editor";
 
 function App() {
-  return (
-    <div className="App">
-
-    </div>
-  );
+    return (
+        <div className="App">
+            <Editor />
+        </div>
+    );
 }
 
 export default App;
---
+++ src/components/Editor.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Editor() {
+    return <textarea name="" id="" cols="30" rows="10"></textarea>;
+}
+
+export default Editor;
