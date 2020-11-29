5931e88f4ba8563d72c1ab35105830d1c0e2e038
Sebastian Arrieta <sarrietav@unicartagena.edu.co>
2020-11-28 21:34:36 -0500

    Added Previewer component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import Editor from "./components/Editor";
+import Previewer from "./components/Previewer";
 
 function App() {
     return (
         <div className="App">
             <Editor />
+            <Previewer />
         </div>
     );
 }
---
+++ src/components/Previewer.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Previewer() {
+    return <div></div>;
+}
+
+export default Previewer;
