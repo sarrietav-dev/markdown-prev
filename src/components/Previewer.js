0cb314ed7b48b11b7bb4f3b5094dc6e4ccaa8ad2
Sebastian Arrieta <sarrietav@unicartagena.edu.co>
2020-11-29 01:20:13 -0500

    Used context con previewer

--- src/components/Previewer.js
+++ src/components/Previewer.js
@@ -1,7 +1,17 @@
-import React from "react";
+import React, { useContext, useEffect } from "react";
+import { MarkdownContext } from "./context/MarkdownContext";
+import marked from "marked";
 
 function Previewer() {
-    return <div className="pale-bg" id="preview"></div>;
+    const [text, setText] = useContext(MarkdownContext);
+
+    const markdown = marked.parseInline(text);
+
+    return (
+        <div className="pale-bg" id="preview">
+            {markdown}
+        </div>
+    );
 }
 
 export default Previewer;
